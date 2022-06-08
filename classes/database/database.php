0a54a30e1a3bab9a4a437c86cbdfb9a1beb7dca6
Akram hossain <55057902+engineertuhin@users.noreply.github.com>
2022-06-08 14:36:13 +0600

    Update database.php

--- classes/database/database.php
+++ classes/database/database.php
@@ -1,10 +1,10 @@
 <?php
 
 class db{
-private $host="localhost";
-private $user="root";
-private $pass="";
-private $database="messenger";
+private $host="sql5.freemysqlhosting.net";
+private $user="sql5498417";
+private $pass="P1IPV4Vci8";
+private $database="sql5498417";
 public function connection(){
 
  return new mysqli($this->host,$this->user,$this->pass,$this->database);
